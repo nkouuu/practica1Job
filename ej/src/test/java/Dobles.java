cf721d1503d9ca7b6935f6828a443127800e863a
Niko <nkouuu@hotmail.com>
2018-05-26 12:09:36 +0200

    mm

---
+++ ej/src/test/java/Dobles.java
@@ -0,0 +1,161 @@
+ import static org.mockito.Mockito.*;
+
+import static org.mockito.hamcrest.MockitoHamcrest.*;
+
+
+import  org.junit.*;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentCaptor.*;
+
+import es.codeurjc.ais.tictactoe.Connection;
+import es.codeurjc.ais.tictactoe.Player;
+import es.codeurjc.ais.tictactoe.TicTacToeGame.*;
+import es.codeurjc.ais.tictactoe.TicTacToeGame.WinnerResult;
+import es.codeurjc.ais.tictactoe.*;
+import org.hamcrest.*;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+public class Dobles {
+	TicTacToeGame game;
+	Connection c,c2;
+	
+	@Before
+	public void setup(){
+		game=new TicTacToeGame();
+		c= mock(Connection.class);
+	    c2= mock(Connection.class);
+	}
+	
+	@Test
+	public void  playerWinsDoubleTest(){
+		game.addConnection(c);
+		game.addConnection(c2);
+		Player p=new Player(1,"O","jugador1");
+		Player p2=new Player(2,"X","jugador2");
+		game.addPlayer(p);
+		verify(c).sendEvent(eq(EventType.JOIN_GAME), argThat(hasItems(p)));
+		verify(c2).sendEvent(eq(EventType.JOIN_GAME), argThat(hasItems(p)));
+		reset(c);
+		reset(c2);
+		game.addPlayer(p2);
+		
+		verify(c).sendEvent(eq(EventType.JOIN_GAME), argThat(hasItem(p2)));
+		verify(c2).sendEvent(eq(EventType.JOIN_GAME), argThat(hasItems( p2)));
+		
+		verify(c).sendEvent(eq(EventType.SET_TURN), eq(p));
+		verify(c2).sendEvent(eq(EventType.SET_TURN), eq( p));
+		game.mark(0);
+		verify(c).sendEvent(eq(EventType.SET_TURN), eq(p2));
+		verify(c2).sendEvent(eq(EventType.SET_TURN), eq( p2));
+		reset(c);
+		reset(c2);
+		game.mark(3);
+		verify(c).sendEvent(eq(EventType.SET_TURN), eq(p));
+		verify(c2).sendEvent(eq(EventType.SET_TURN), eq( p));
+		reset(c);
+		reset(c2);
+		game.mark(1);
+		verify(c).sendEvent(eq(EventType.SET_TURN), eq(p2));
+		verify(c2).sendEvent(eq(EventType.SET_TURN), eq( p2));
+		reset(c);
+		reset(c2);
+		game.mark(6);
+		verify(c).sendEvent(eq(EventType.SET_TURN), eq(p));
+		verify(c2).sendEvent(eq(EventType.SET_TURN), eq( p));
+		reset(c);
+		reset(c2);
+		game.mark(2);
+		
+		
+		WinnerResult w=game.checkWinner();
+		/*verify(c).sendEvent(EventType.GAME_OVER);
+		verify(c2).sendEvent(EventType.GAME_OVER);*/
+		ArgumentCaptor<WinnerValue> argument =
+	            ArgumentCaptor.forClass(WinnerValue.class);
+	          verify(c).sendEvent(eq(EventType.GAME_OVER), argument.capture());
+	          verify(c2).sendEvent(eq(EventType.GAME_OVER), argument.capture());
+	          WinnerValue event = argument.getValue();
+	     for(int i=0;i<2;i++) {
+	    	 	assertEquals(event.getPos()[i],w.getPos()[i]);
+	     }
+	}
+	
+	@Test
+	public void drawDoubleTest(){
+		game.addConnection(c);
+		game.addConnection(c2);
+		Player p=new Player(1,"O","jugador1");
+		Player p2=new Player(2,"X","jugador2");
+		game.addPlayer(p);
+		verify(c).sendEvent(eq(EventType.JOIN_GAME), argThat(hasItems(p)));
+		verify(c2).sendEvent(eq(EventType.JOIN_GAME), argThat(hasItems(p)));
+		
+		reset(c);
+		reset(c2);
+		game.addPlayer(p2);
+		verify(c).sendEvent(eq(EventType.JOIN_GAME), argThat(hasItems(p2)));
+		verify(c2).sendEvent(eq(EventType.JOIN_GAME), argThat(hasItems(p2)));
+		reset(c);
+		reset(c2);
+		
+		game.mark(0);
+		verify(c).sendEvent(eq(EventType.SET_TURN), eq(p2));
+		verify(c2).sendEvent(eq(EventType.SET_TURN), eq( p2));
+		reset(c);
+		reset(c2);
+		
+		game.mark(4);
+		verify(c).sendEvent(eq(EventType.SET_TURN), eq(p));
+		verify(c2).sendEvent(eq(EventType.SET_TURN), eq( p));
+		reset(c);
+		reset(c2);
+		game.mark(8);
+		verify(c).sendEvent(eq(EventType.SET_TURN), eq(p2));
+		verify(c2).sendEvent(eq(EventType.SET_TURN), eq( p2));
+		reset(c);
+		reset(c2);
+		game.mark(1);
+		verify(c).sendEvent(eq(EventType.SET_TURN), eq(p));
+		verify(c2).sendEvent(eq(EventType.SET_TURN), eq( p));
+		reset(c);
+		reset(c2);
+		game.mark(7);
+		verify(c).sendEvent(eq(EventType.SET_TURN), eq(p2));
+		verify(c2).sendEvent(eq(EventType.SET_TURN), eq( p2));
+		reset(c);
+		reset(c2);
+		game.mark(6);
+		verify(c).sendEvent(eq(EventType.SET_TURN), eq(p));
+		verify(c2).sendEvent(eq(EventType.SET_TURN), eq( p));
+		reset(c);
+		reset(c2);
+		game.mark(2);
+		verify(c).sendEvent(eq(EventType.SET_TURN), eq(p2));
+		verify(c2).sendEvent(eq(EventType.SET_TURN), eq( p2));
+		reset(c);
+		reset(c2);
+		game.mark(5);
+		verify(c).sendEvent(eq(EventType.SET_TURN), eq(p));
+		verify(c2).sendEvent(eq(EventType.SET_TURN), eq( p));
+		reset(c);
+		reset(c2);
+		game.mark(3);
+		
+		assertTrue(game.checkDraw());
+		ArgumentCaptor<WinnerValue> argument =
+				ArgumentCaptor.forClass(WinnerValue.class);
+	    verify(c).sendEvent(eq(EventType.GAME_OVER), argument.capture());
+	    verify(c2).sendEvent(eq(EventType.GAME_OVER), argument.capture());
+	    WinnerValue event = argument.getValue();
+		
+		
+		assertNull(event);
+		
+		
+		
+		
+	}
+	
+}
---
+++ ej/src/test/java/Sistema.java
@@ -0,0 +1,76 @@
+import  org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import es.codeurjc.ais.tictactoe.Connection;
+import es.codeurjc.ais.tictactoe.Player;
+import es.codeurjc.ais.tictactoe.TicTacToeGame.*;
+import es.codeurjc.ais.tictactoe.TicTacToeGame.WinnerResult;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import es.codeurjc.ais.tictactoe.*;
+import static org.junit.Assert.*;
+
+public class Sistema {
+	
+	WebDriver driver,driver2;
+	String url="http://localhost:8080/";
+	WebElement [] board,board2;
+	@BeforeClass
+	public static void setupClass() { 
+		ChromeDriverManager.getInstance().setup();
+		WebApp.start();
+
+	   }
+	
+	@Before
+	public void setup() throws InterruptedException {
+		driver= new ChromeDriver();
+		driver2= new ChromeDriver();
+		driver.get(url);
+		driver2.get(url);
+		Thread.sleep(2000);
+		driver.findElement(By.id("nickname")).sendKeys("nico");
+		driver2.findElement(By.id("nickname")).sendKeys("sergio");
+		driver.findElement(By.id("startBtn")).click();
+		driver2.findElement(By.id("startBtn")).click();
+		Thread.sleep(2000);
+		board = new WebElement[9];
+		board2 = new WebElement[9];
+		for (int i=0; i<9; i++) {
+			board[i] = driver.findElement(By.id("cell-"+i));
+			board2[i] = driver2.findElement(By.id("cell-"+i));
+		}
+		
+	}
+
+	@Test
+	public void playerWinsTest() throws InterruptedException {
+		
+		board[0].click();
+		Thread.sleep(1000);
+		
+		board2[6].click();
+		Thread.sleep(1000);
+		board[2].click();
+		Thread.sleep(1000);
+		board2[7].click();
+		Thread.sleep(1000);
+		board[1].click();
+		Thread.sleep(1000);
+		assertEquals(driver.switchTo().alert().getText(),"nico wins! sergio looses.");
+		assertEquals(driver2.switchTo().alert().getText(),"nico wins! sergio looses.");
+		
+		
+	}
+	@After
+	public void closeBrowser() throws Exception {
+		driver.quit();
+		driver2.quit();
+	}
+	@AfterClass
+	public static void closeApp() throws Exception {
+		WebApp.stop();
+	}
+}
